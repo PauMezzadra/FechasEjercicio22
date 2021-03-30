cdb95a7b92ac9fb5d3a5f764b450492b8ebc407c
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-30 14:44:54 -0300

    Add project files.

---
+++ FechasEjercicio22/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FechasEjercicio22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hoy es {DateTime.Now.Day} del mes {DateTime.Now.Month} del año {DateTime.Now.Year}");
+            Console.ReadKey();
+        }
+    }
+}
